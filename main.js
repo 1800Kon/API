e2528d6c0e2b0120b3fdab439dea0af9aef3f6d0
KBG360 <55129891+KBG360@users.noreply.github.com>
2021-01-15 12:55:45 +0100

    fixes and add schemas

--- main.js
+++ main.js
@@ -3,18 +3,22 @@ var mysql = require('mysql');
 var validator = require('validator')
 var app = express();
 var validate = require('jsonschema').validate;
+var libxml = require("libxmljs2");
 app.use(express.json());
+
+//Constants
 const busiestAirportsSchema = require('./busiest_airports_schema.json')
+const busiestAirportsSchemaGet = require('./busiest_airport_schema_get.json')
 const busiestAirportsSchemaUpdate = require('./busiest_airports_update_schema.json')
 const busiestAirportsSchemaDelete = require('./busiest_airport_schema_delete.json')
-//Database connection
+
+//Database connection, could use connection pooling to fix the issue with having to reinstantiate the connection
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "api_database_dp"
 });
-
 
 //Function to connect to the database
 function connectToDB() {
@@ -26,51 +30,66 @@ function connectToDB() {
     });
 }
 
-//Function to close a connection
-function endconnection() {
-    connection.end(function(err) {
-        if (err) {
-          return console.log('error:' + err.message);
-        }
-        console.log('Closed the database connection.');
-      });
-}
-
-
-//App.get represents that this is a get request, the "/" is the path at which the api will respond with the response.send("hello").
+//Main page
 app.get('/', (req, res) => {
     res.send("This is the main page where I'll put an index");
 })
 
 //Get the busiest airports based on rank and year
-app.get('/busiestAirports/rank/:rank/:year', (req, res) => {
-    var rank = req.params.rank;
-    var year = req.params.year;
+app.get('/busiestAirports/getRankOfYear', (req, res) => {
+    //Create the connection
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "api_database_dp"
+    });
+    //Query to be executed
     var sql = "SELECT * FROM busiest_airports WHERE rank = ? AND year = ?";
-    //Checks if the parameters only have numbers
-    if (/^\d+$/.test(rank) && /^\d+$/.test(year)) {
-        if (connectToDB) {
-            //Values automatically escaped
-            connection.query(sql, [rank, year], function (err, result) {
-                if (err) throw err;
-                //Check if there were any results
-                if (result < 1) {
-                    res.status(404).send("No data found for these parameters.")
-                    endconnection()
-                } else {
-                    res.send(result);
-                    endconnection()
-                }
-            });
-        }
+    var jsonInput = req.body;
+    //Validates the results against the schema
+    var result = validate(jsonInput, busiestAirportsSchemaGet);
+    //Checks if the postman input is of type json
+    if (req.get("Content-Type") != "application/json") {
+        res.status(400).send("Please use Json")
     } else {
-        res.send("Please only use digits in the query.");
+        //Checks if the results are valid or not
+        if (result.valid) {
+            //Connects to the database
+            if (connectToDB) {
+                //Values automatically escaped
+                connection.query(sql, [jsonInput.rank, jsonInput.year], function (err, result) {
+                    if (err) {
+                        res.send(err)
+                    }
+                    //Check if there were any results
+                    if (result < 1) {
+                        res.status(404).send("No data found for these parameters.")
+                    } else {
+                        //Send the results
+                        res.send(result);
+                    }
+                });
+                //End the connection after the transaction is complete
+                connection.end();
+            } else {
+                console.log("Error connecting to the database.")
+            }
+        } else {
+            res.status(400).send(result);
+        }
     }
-
 });
 
 //Add a record to the busiest airport table
 app.post('/busiestAirports/addEntry', (req, res) => {
+    //Connection
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "api_database_dp"
+    });
     var sql = "INSERT INTO busiest_airports (rank, year, airport, code, location, country, total_passengers) VALUES (?,?,?,?,?,?,?)";
     if (req.get("Content-Type") != "application/json") {
         res.status(400).send("Please use Json")
@@ -86,11 +105,10 @@ app.post('/busiestAirports/addEntry', (req, res) => {
                     connection.query(sql, array, function (err, result) {
                         if (err) {
                             res.send(result);
-                            endconnection()
+                            connection.end();
                         } else {
                             res.send("Data has been inserted.")
-                            //Close the connection each time
-                            endconnection()
+                            connection.end();
                         }
                     });
 
@@ -106,6 +124,13 @@ app.post('/busiestAirports/addEntry', (req, res) => {
 
 //Update the rank of a single airport
 app.put('/busiestAirports/updateSingleRank', (req, res) => {
+    //Connection
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "api_database_dp"
+    });
     var sql = "UPDATE busiest_airports SET rank = ? WHERE airport = ? AND ID = ? AND total_passengers = ?";
     if (req.get("Content-Type") != "application/json") {
         res.status(400).send("Please use Json")
@@ -119,14 +144,14 @@ app.put('/busiestAirports/updateSingleRank', (req, res) => {
                     connection.query(sql, array, function (err, result) {
                         if (err) {
                             res.send(err.message)
-                            endconnection()
+                            connection.end();
                         } else {
                             if (result.affectedRows > 0) {
                                 res.send("The row has been succesfully updated")
-                                endconnection()
+                                connection.end();
                             } else {
                                 res.status(404).send("No entries were found with the parameters provided")
-                                endconnection()
+                                connection.end();
                             }
                         }
                     });
@@ -143,6 +168,12 @@ app.put('/busiestAirports/updateSingleRank', (req, res) => {
 
 //Delete a record from the busiestAirports table
 app.delete('/busiestAirports/deleteEntry', (req, res) => {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "api_database_dp"
+    });
     var sql = "DELETE FROM busiest_airports WHERE ID = ? AND airport = ?";
     if (req.get("Content-Type") != "application/json") {
         res.status(400).send("Please use Json")
@@ -156,14 +187,14 @@ app.delete('/busiestAirports/deleteEntry', (req, res) => {
                     connection.query(sql, array, function (err, result) {
                         if (err) {
                             res.send(err.message)
-                            endconnection()
+                            connection.end();
                         } else {
                             if (result.affectedRows > 0) {
                                 res.send("The entry has been succesfully deleted")
-                                endconnection()
+                                connection.end();
                             } else {
                                 res.status(404).send("No entries were found with the parameters provided")
-                                endconnection()
+                                connection.end();
                             }
                         }
                     });
@@ -177,8 +208,7 @@ app.delete('/busiestAirports/deleteEntry', (req, res) => {
     }
 })
 
-//Get the number of negative or positive reviews for an airline
 
-
+//Dinamically change the port in a working environment
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
